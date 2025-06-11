4a9961eecf846282aad4274306d764fee84d7b5b
chmtrip59 <chmtrip59@gmail.com>
2025-06-11 12:05:46 +0530

    newChanges

---
+++ force-app/main/default/lwc/importUtility/importUtility.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import MYLOGO from "@salesforce/resourceUrl/myImg"
+
+
+export default class ImportUtility extends LightningElement {
+
+
+
+    myNewLogo = MYLOGO;
+}
