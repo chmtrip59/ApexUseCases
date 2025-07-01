49b5fa5d8c7b07f0660529b271e3bce455ba4e0e
chmtrip59 <chmtrip59@gmail.com>
2025-07-01 17:48:52 +0530

    LDS - lightning record form page

---
+++ force-app/main/default/lwc/lightningRecordPage/lightningRecordPage.js
@@ -0,0 +1,12 @@
+import LeadSource from '@salesforce/schema/Contact.LeadSource';
+import { LightningElement,api } from 'lwc';
+
+export default class LightningRecordPage extends LightningElement {
+    // recordId = "500NS00000yUwfKYAS";
+    // for record page if i put this on case record page it will same record for every other case id , to tackle this we can use recordId with api decorator
+
+    @api recordId;
+    objectApiName = 'Case';
+    fields = ['CaseOwner','CaseNumber','Status' , 'Origin' , 'Priority' ,'Reason','Type'];
+    
+}
